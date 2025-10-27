2bfb40ce0b1b385e35863f6f7f924dfd03924af5
Haodong Zhang <haodoz0118@gmail.com>
2025-10-27 12:34:53 -0400

    Used Layer Norm

--- model/backbone.py
+++ model/backbone.py
@@ -3,24 +3,39 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class LayerNorm1d(nn.Module):
+    def __init__(self, num_channels, eps=1e-5, affine=True):
+        super().__init__()
+        self.ln = nn.LayerNorm(num_channels, eps=eps, elementwise_affine=affine)
+
+    def forward(self, x):  # x: [B, C, T]
+        x = x.transpose(1, 2)      # [B, T, C]
+        x = self.ln(x)
+        return x.transpose(1, 2)   # [B, C, T]
+
 class ExtractEncoder(nn.Module):
     def __init__(self, in_chan):
         super().__init__()
         self.encoder = nn.Sequential(
             nn.Conv1d(in_chan, 512, kernel_size=8, stride=5, padding=0, bias=False),
-            nn.BatchNorm1d(512),
+            # nn.BatchNorm1d(512),
+            LayerNorm1d(512),
             nn.ReLU(inplace=True),
             nn.Conv1d(512, 512, kernel_size=8, stride=5, padding=0, bias=False),
-            nn.BatchNorm1d(512),
+            # nn.BatchNorm1d(512),
+            LayerNorm1d(512),
             nn.ReLU(inplace=True),
             nn.Conv1d(512, 512, kernel_size=4, stride=3, padding=0, bias=False),
-            nn.BatchNorm1d(512),
+            # nn.BatchNorm1d(512),
+            LayerNorm1d(512),
             nn.ReLU(inplace=True),
             nn.Conv1d(512, 512, kernel_size=3, stride=1, padding=0, bias=False),
-            nn.BatchNorm1d(512),
+            # nn.BatchNorm1d(512),
+            LayerNorm1d(512),
             nn.ReLU(inplace=True),
             nn.Conv1d(512, 512, kernel_size=3, stride=1, padding=0, bias=False),
-            nn.BatchNorm1d(512),
+            # nn.BatchNorm1d(512),
+            LayerNorm1d(512),
             nn.ReLU(inplace=True),
         )
 
