69b2ca3edddd763f1f7bbd4ea6f9804452843b59
haodong <haodoz0118@gmail.com>
2025-10-29 18:57:56 +0000

    Fixed the bug

--- tsne_vis.py
+++ tsne_vis.py
@@ -263,8 +263,8 @@ def plot_tsne_grid(encoder_chunk, gru_chunk, encoder_frame, gru_frame,
 
 def main():
     # Configuration
-    method = "GN-LN-256"
-    CHECKPOINT_PATH = f"/root/stable_pretraining_cpc_sample/checkpoints/{method}/last.ckpt"
+    method = "BN"
+    CHECKPOINT_PATH = f"/root/stable_pretraining_cpc_sample/checkpoints/{method}/best.ckpt"
     CONFIG_PATH = "configs/cpc.yaml"
     NUM_POSITIVE = 100
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
